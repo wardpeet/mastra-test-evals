63e564deed9b5d00776cd0fb16a822d5cc54ae41
Ward Peeters <ward@coding-tech.com>
2025-02-12 14:50:44 +0100

    update

--- src/mastra/agents/index.ts
+++ src/mastra/agents/index.ts
@@ -1,5 +1,5 @@
 import { openai } from "@ai-sdk/openai";
-import { Agent } from "@mastra/core/agent";
+import { Agent } from "@mastra/core";
 import { ToneConsistencyMetric } from "@mastra/evals/nlp";
 
 export const weatherAgent = new Agent({
